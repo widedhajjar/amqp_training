d436dcd212ca105ae99fb3e81506c7c8a6587a39
widedhajjar <hajjar@et.esiea.fr>
2021-12-09 14:56:06 +0100

    Part 4

--- src/main/java/fr/lernejo/chat/ChatMessageListener.java
+++ src/main/java/fr/lernejo/chat/ChatMessageListener.java
@@ -1,12 +1,11 @@
 package fr.lernejo.chat;
 
-
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class ChatMessageListener {
+    public static ChatMessageRepository repo = new ChatMessageRepository();
     public void onMessage(String message){
-        System.out.println(message);
+        repo.addChatMessage(message);
     }
 }
